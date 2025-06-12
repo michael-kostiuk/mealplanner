2274a0287902bd0346314e141b2560cd0c6dc972
Kostiuk Michael <10185391+michael-kostiuk@users.noreply.github.com>
2025-06-12 09:41:35 +0200

    Update database.py with pool pre ping an expire

--- app/database.py
+++ app/database.py
@@ -6,7 +6,11 @@ from .models import Base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=900,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
